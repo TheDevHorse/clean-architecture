e772569be66718860fbe148136e9e4293bcaf0ef
hugop <hugopessego@gmail.com>
2024-11-07 11:26:52 +0000

    Add repository port to AthleteController, refine architectural tests
    Introduced `AthleteRepositoryOutputPort` to `AthleteController` for improved dependency management. Enhanced architecture tests to enforce stricter layer access rules and dependencies in the application.

--- src/test/java/com/thedevhorse/cleanarchitecture/CleanArchitectureTest.java
+++ src/test/java/com/thedevhorse/cleanarchitecture/CleanArchitectureTest.java
@@ -5,20 +5,30 @@ import com.tngtech.archunit.core.importer.ClassFileImporter;
 import com.tngtech.archunit.library.Architectures;
 import org.junit.jupiter.api.Test;
 
+import static com.tngtech.archunit.core.domain.JavaClass.Predicates.resideInAPackage;
 import static com.tngtech.archunit.library.Architectures.layeredArchitecture;
 
 class CleanArchitectureTest {
 
     @Test
-    void givenControllerClasses_thenNotBeAccessedByAnyLayer() {
+    void givenCleanArchitectureClasses_thenLayerDependenciesAreRespected() {
         JavaClasses importedClasses = new ClassFileImporter().importPackages("com.thedevhorse.cleanarchitecture");
 
-        Architectures.LayeredArchitecture arch = layeredArchitecture()
-                .consideringAllDependencies()
-                .layer("Controller").definedBy("..controller..")
+        Architectures.LayeredArchitecture architectureRules = layeredArchitecture()
+                .consideringOnlyDependenciesInLayers()
+                .layer("UseCase").definedBy("..usecase")
+                .layer("InputPort").definedBy("..usecase.port.in")
+                .layer("OutputPort").definedBy("..usecase.port.out")
+                .layer("Infra").definedBy("..infra..")
 
-                .whereLayer("Controller").mayNotBeAccessedByAnyLayer();
+                .whereLayer("UseCase").mayOnlyBeAccessedByLayers("InputPort")
+                .whereLayer("UseCase").mayOnlyAccessLayers("OutputPort")
+                .whereLayer("InputPort").mayOnlyBeAccessedByLayers("Infra")
+                .whereLayer("OutputPort").mayOnlyBeAccessedByLayers("UseCase")
 
-        arch.check(importedClasses);
+                .ignoreDependency(resideInAPackage("..infra.."), resideInAPackage("..usecase.port.out.."))
+                .ignoreDependency(resideInAPackage("..usecase"), resideInAPackage("..usecase.port.in.."));
+
+        architectureRules.check(importedClasses);
     }
 }
